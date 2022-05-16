8c2f6784bcc0e7c7ed2e6abf9af6c04ea5617713
Onomesino Esther <onomesinoesther@Onomesinos-MacBook-Pro.local>
2022-05-16 17:22:12 +0000

    doc

---
+++ Document_files/myscript.js
@@ -0,0 +1,25 @@
+let old =90;
+let young = 20;
+
+console.log(90 - 20);
+
+var yz = 100;
+var ew = 200;
+
+console.log(100 + 200);
+
+var sam = 20;
+var ab = 5;
+
+console.log(20*5);
+
+let me = 50;
+let out = 30;
+
+console.log(50/30);
+
+var up =70;
+var it =70;
+
+console.log(70*70);
+ 
